a6f7971018b0c083c6c5ffcf8a3fcd0edee27e4c
Zemelua <zemelua1107@gmail.com>
2024-01-10 23:55:46 +0900

    コンフィグを実装

---
+++ src/main/java/io/github/zemelua/nade_nade/client/ModClientConfig.java
@@ -0,0 +1,55 @@
+package io.github.zemelua.nade_nade.client;
+
+import io.github.zemelua.nade_nade.NadeNade;
+import io.github.zemelua.umu_config.api.config.container.ConfigContainer;
+import io.github.zemelua.umu_config.api.config.container.IConfigContainer;
+import io.github.zemelua.umu_config.api.config.value.BooleanConfigValue;
+import io.github.zemelua.umu_config.api.config.value.IConfigValue;
+import io.github.zemelua.umu_config.api.util.UMUConfigClothUtils;
+import me.shedaniel.clothconfig2.api.ConfigBuilder;
+import me.shedaniel.clothconfig2.api.ConfigCategory;
+import me.shedaniel.clothconfig2.api.Requirement;
+import me.shedaniel.clothconfig2.gui.entries.BooleanListEntry;
+import net.minecraft.text.Text;
+
+public class ModClientConfig {
+	private final IConfigValue<Boolean> useAttackKey;
+	private final IConfigValue<Boolean> useAdditionalKey;
+	private final IConfigContainer container;
+
+	protected ModClientConfig() {
+		this.useAttackKey = BooleanConfigValue.builder(NadeNade.identifier("use_attack_key"))
+				.defaultValue(true)
+				.build();
+		this.useAdditionalKey = BooleanConfigValue.builder(NadeNade.identifier("use_additional_key"))
+				.defaultValue(true)
+				.build();
+		this.container = ConfigContainer.builder(NadeNade.identifier("nade_nade_client"))
+				.addValue(this.useAttackKey)
+				.addValue(this.useAttackKey)
+				.build();
+	}
+
+	public boolean useAttackKey() {
+		return this.useAttackKey.getValue();
+	}
+
+	public boolean useAdditionalKey() {
+		return this.useAdditionalKey.getValue();
+	}
+
+	public IConfigContainer getContainer() {
+		return this.container;
+	}
+
+	public ConfigCategory asScreen(ConfigBuilder builder, ConfigCategory category) {
+		BooleanListEntry useAdditionalKeyEntry = UMUConfigClothUtils.toggle(builder, this.useAdditionalKey).build();
+
+		return category.addEntry(UMUConfigClothUtils.toggle(builder, this.useAttackKey).build())
+				.addEntry(useAdditionalKeyEntry)
+				.addEntry(builder.entryBuilder().fillKeybindingField(Text.translatable(NadeNadeClient.KEY_HEADPAT.getTranslationKey()), NadeNadeClient.KEY_HEADPAT)
+						.setDefaultValue(NadeNadeClient.KEY_HEADPAT.getDefaultKey())
+						.setRequirement(Requirement.isTrue(useAdditionalKeyEntry))
+						.build());
+	}
+}
--- src/main/java/io/github/zemelua/nade_nade/client/NadeNadeClient.java
+++ src/main/java/io/github/zemelua/nade_nade/client/NadeNadeClient.java
@@ -1,14 +1,24 @@
 package io.github.zemelua.nade_nade.client;
 
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
 import io.github.zemelua.nade_nade.client.event.ClientCallbacks;
+import io.github.zemelua.umu_config.api.config.IConfigProvider;
+import io.github.zemelua.umu_config.api.config.container.IConfigContainer;
+import io.github.zemelua.umu_config.config.ConfigFileManager;
+import me.shedaniel.clothconfig2.api.ConfigBuilder;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.event.client.player.ClientPreAttackCallback;
 import net.minecraft.client.option.KeyBinding;
+import net.minecraft.text.Text;
 import org.lwjgl.glfw.GLFW;
 
-public class NadeNadeClient implements ClientModInitializer {
+import java.util.List;
+
+public class NadeNadeClient implements ClientModInitializer, IConfigProvider, ModMenuApi {
+	public static final ModClientConfig CONFIG = new ModClientConfig();
 	public static final KeyBinding KEY_HEADPAT = new KeyBinding("key.nade_nade.headpat", GLFW.GLFW_KEY_P, KeyBinding.GAMEPLAY_CATEGORY);
 
 	@Override
@@ -17,5 +27,25 @@ public class NadeNadeClient implements ClientModInitializer {
 
 		ClientTickEvents.START_CLIENT_TICK.register(ClientCallbacks::onStartTick);
 		ClientPreAttackCallback.EVENT.register((client, player, clickCount) -> ClientCallbacks.preAttackInput(player));
+	}
+
+	@Override
+	public List<IConfigContainer> getConfigs() {
+		return List.of(CONFIG.getContainer());
+	}
+
+	@Override
+	public ConfigScreenFactory<?> getModConfigScreenFactory() {
+		return parent -> {
+				ConfigBuilder builder = ConfigBuilder.create();
+
+				CONFIG.asScreen(builder, builder.getOrCreateCategory(CONFIG.getContainer().getName()));
+
+				builder.setParentScreen(parent)
+						.setTitle(Text.literal("Nade-Nade Config"))
+						.setSavingRunnable(() -> ConfigFileManager.saveFrom(CONFIG.getContainer()));
+
+				return builder.build();
+		};
 	}
 }
